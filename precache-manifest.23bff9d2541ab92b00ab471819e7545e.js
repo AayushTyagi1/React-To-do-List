e09cc3d5341ac4ba564df307057c94a2f998c342
Aayush Tyagi <tyagiaayush5@gmail.com>
2020-07-27 14:00:34 +0530

    Updates

---
+++ precache-manifest.23bff9d2541ab92b00ab471819e7545e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c04c74b33490c10e91054c4da191d1",
+    "url": "/React-To-do-List/index.html"
+  },
+  {
+    "revision": "96944f1dfa6010d3f4e1",
+    "url": "/React-To-do-List/static/css/main.9aac00de.chunk.css"
+  },
+  {
+    "revision": "0a6d262afecf3ae2b260",
+    "url": "/React-To-do-List/static/js/2.e01fc029.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-To-do-List/static/js/2.e01fc029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96944f1dfa6010d3f4e1",
+    "url": "/React-To-do-List/static/js/main.ebe3bacc.chunk.js"
+  },
+  {
+    "revision": "bc8438f6ff2a4699b14d",
+    "url": "/React-To-do-List/static/js/runtime-main.656c2db9.js"
+  }
+]);
